7349bb0adfbfe4faaad8cc6a0c795de5ed71c68b
machawidajaj <soufyane.fekhar@gmail.com>
2026-07-25 09:20:18 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='machawi-stock-v12-final-login';
+const CACHE='machawi-stock-v14-orders-notifications';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-maskable-512.png','./favicon.png','./app-icon.png'];
 
 self.addEventListener('install',event=>{
